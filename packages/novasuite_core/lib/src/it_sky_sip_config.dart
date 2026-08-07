d9ed2de6c1f2f87c29bba0018311cbf02e1ee3dd
joelodufu <joelodufu@gmail.comm>
2026-08-07 16:40:37 +0100

    fix(it-sky-config): set exact WSS URL to wss://astpp.itskysolutions.com:7443

--- packages/novasuite_core/lib/src/it_sky_sip_config.dart
+++ packages/novasuite_core/lib/src/it_sky_sip_config.dart
@@ -54,7 +54,7 @@ class ItSkySipConfig {
   /// Signaling Ports & WebRTC Proxy Endpoints
   static const int udpSignalingPort = 5060;
   static const int wssWebRtcPort = 7443;
-  static const String wssPort7443Url = 'wss://astpp.itskysolutions.com:7443/ws';
+  static const String wssPort7443Url = 'wss://astpp.itskysolutions.com:7443';
   static const String wssWebRtcUrl = 'wss://07003100077.astpp.itskysolutions.com:8089/ws';
   static const String wsWebRtcUrl = 'ws://07003100077.astpp.itskysolutions.com:8089/ws';
   static const String wsDirectIpUrl = 'ws://95.217.244.97:8089/ws';
